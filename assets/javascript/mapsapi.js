724382c2202165ab19bd7f7474688a396d2a2351
Eric Jobin <ejobin@gmail.com>
2019-07-23 13:29:11 -0400

    Added functionality to maps, will now locate user's position, default to Toronto if permission denied

--- assets/javascript/mapsapi.js
+++ assets/javascript/mapsapi.js
@@ -9,10 +9,49 @@ $(document).ready(function() {
     });
 });
 
-var map;
+// var map;
+// function initMap() {
+//     map = new google.maps.Map(document.getElementById('map'), {
+//         center: {lat: -34.397, lng: 150.644},
+//         zoom: 8 // 1 World, 10 Cities, 20 Buildings
+//     });
+// }
+
+var map, infoWindow;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8
+        center: {lat: 43.653, lng: -79.383}, //Location will default to Toronto if browser does not support
+        zoom: 14
     });
+    infoWindow = new google.maps.InfoWindow;
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        };
+
+        infoWindow.setPosition(pos);
+        infoWindow.setContent('Location found.');
+        infoWindow.open(map);
+        map.setCenter(pos);
+        }, 
+        function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } 
+    else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+                            'Error: The Geolocation service failed.' :
+                            'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
 }
