6a8c906678de743260da5b2dc927bf7143296eea
Eric Jobin <ejobin@gmail.com>
2019-07-24 11:41:06 -0400

    Broke out js form maps html file

---
+++ assets/javascript/mapsapi.js
@@ -0,0 +1,40 @@
+var map;
+var service;
+var infowindow;
+
+function initMap() {
+    var sydney = new google.maps.LatLng(43.653, -79.383);
+
+    infowindow = new google.maps.InfoWindow();
+
+    map = new google.maps.Map(
+        document.getElementById('map'), {center: sydney, zoom: 15});
+
+    var request = {
+        query: 'Loblaws',
+        fields: ['name', 'geometry'],
+    };
+
+    service = new google.maps.places.PlacesService(map);
+
+    service.findPlaceFromQuery(request, function(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < results.length; i++) {
+                createMarker(results[i]);
+            }
+            map.setCenter(results[0].geometry.location);
+        }
+    });
+}
+
+function createMarker(place) {
+    var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent(place.name);
+        infowindow.open(map, this);
+    });
+}
