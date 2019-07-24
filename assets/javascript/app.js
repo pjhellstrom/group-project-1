7fa4cdab5e7107428c69652bff23069202779bf3
Jonas H <pjh.hellstrom@gmail.com>
2019-07-24 16:26:22 -0400

    Moved Maps API code block to inside doc.ready for jQuery

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -253,49 +253,50 @@ function listHealthLabels(i, size) {
 // return chart;
 // }//end createChart
 
-});//end $(document)ready()
-
 // Maps API
 $(document).on("click", "#plusBtn", function() {
    
-$("#mapHide").show();
-
-var map;
-var service;
-var infowindow;
-
-function initMap() {
-    var toronto = new google.maps.LatLng(43.653, -79.383);
-
-    infowindow = new google.maps.InfoWindow();
-
-    map = new google.maps.Map(document.getElementById('map'), {center: toronto, zoom: 15});
-
-    var request = {
-        query: 'Loblaws',
-        fields: ['name', 'geometry'],
-    };
-
-    service = new google.maps.places.PlacesService(map);
-    service.findPlaceFromQuery(request, function(results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-            for (var i = 0; i < results.length; i++) {
-                createMarker(results[i]);
+    $("#mapHide").show();
+    
+    var map;
+    var service;
+    var infowindow;
+    
+    function initMap() {
+        var toronto = new google.maps.LatLng(43.653, -79.383);
+    
+        infowindow = new google.maps.InfoWindow();
+    
+        map = new google.maps.Map(document.getElementById('map'), {center: toronto, zoom: 15});
+    
+        var request = {
+            query: 'Loblaws',
+            fields: ['name', 'geometry'],
+        };
+    
+        service = new google.maps.places.PlacesService(map);
+        service.findPlaceFromQuery(request, function(results, status) {
+            if (status === google.maps.places.PlacesServiceStatus.OK) {
+                for (var i = 0; i < results.length; i++) {
+                    createMarker(results[i]);
+                }
+                map.setCenter(results[0].geometry.location);
             }
-            map.setCenter(results[0].geometry.location);
-        }
-    });
-}
+        });
+    }
+    
+    function createMarker(place) {
+        var marker = new google.maps.Marker({
+            map: map,
+            position: place.geometry.location
+        });
+    
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.setContent(place.name);
+            infowindow.open(map, this);
+        });
+    };
+    });//end Maps API
+    
+});//end $(document)ready()
 
-function createMarker(place) {
-    var marker = new google.maps.Marker({
-        map: map,
-        position: place.geometry.location
-    });
-
-    google.maps.event.addListener(marker, 'click', function() {
-        infowindow.setContent(place.name);
-        infowindow.open(map, this);
-    });
-};
-});//end Maps API
