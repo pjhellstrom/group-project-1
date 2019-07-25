83608cd3fadb201b9c903dcf16d2d9c588d63b2b
Eric Jobin <ejobin@gmail.com>
2019-07-25 00:05:46 -0400

    Coded maps page to find your location and all nearby supermarkets

--- assets/javascript/simplemap.js
+++ assets/javascript/simplemap.js
@@ -1,38 +1,74 @@
-var map;
-var service;
-var infowindow;
+// Global variables
 
-function initMap() {
-    var toronto = new google.maps.LatLng(43.653, -79.383);
+var map, infoWindow;
+var position = {
+    lat: 43.653,
+    lng: -79.383,
+};
 
-    infowindow = new google.maps.InfoWindow();
-
-    map = new google.maps.Map(document.getElementById('map'), {center: toronto, zoom: 15});
-
-    var request = {
-        query: 'Loblaws',
-        fields: ['name', 'geometry'],
-    };
-
-    service = new google.maps.places.PlacesService(map);
-    service.findPlaceFromQuery(request, function(results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-            for (var i = 0; i < results.length; i++) {
-                createMarker(results[i]);
-            }
-            map.setCenter(results[0].geometry.location);
-        }
-    });
+function handleLocationError (content, position){
+    infoWindow.setPosition(position);
+    infoWindow.setContent(content);
+    infoWindow.open(map);
 }
 
-function createMarker(place) {
-    var marker = new google.maps.Marker({
-        map: map,
-        position: place.geometry.location
-    });
+function initMap() {}
 
-    google.maps.event.addListener(marker, 'click', function() {
-        infowindow.setContent(place.name);
-        infowindow.open(map, this);
-    });
-}
+$(() => {
+    initMap = function() {
+        var options = {
+            center: {lat: 43.653, lng: -79.383},
+            zoom: 14,
+            disableDefaultUI: true,
+        };
+        map = new google.maps.Map(document.getElementById('map'), options);
+        infoWindow = new google.maps.InfoWindow;
+
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (p){
+                position = {
+                    lat: p.coords.latitude,
+                    lng: p.coords.longitude,
+                };
+                infoWindow.setPosition(position);
+                infoWindow.setContent('You are here');
+                infoWindow.open(map);
+
+                var request = {
+                    location: position,
+                    radius: 5000,
+                    types: ['supermarket'],
+                };
+
+                var service = new google.maps.places.PlacesService(map);
+
+                service.nearbySearch(request, callback);
+
+                function callback(results, status){
+                    if(status == google.maps.places.PlacesServiceStatus.OK){
+                        for (var i=0; i< results.length; i++){
+                            createMarker(results[i]);
+                        }
+                    }
+                }
+    
+                function createMarker(place){
+                    var placeLoc = place.geometry.location;
+                    var marker = new google.maps.Marker({
+                        map: map,
+                        position: place.geometry.location
+                    });
+
+                    google.maps.event.addListener(marker, 'click', function() {
+                        infoWindow.setContent(place.name);
+                        infoWindow.open(map, this);
+                    })
+                }
+
+
+            });
+
+        };
+
+    }
+})
